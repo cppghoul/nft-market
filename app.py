393f9b3686ba1392d9f6eae1154665406a4988dc
cppghoul <Matvejgluskov8@gmail.com>
2025-11-29 11:52:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,12 @@ from pyrogram.errors import (
     PhoneCodeExpired,
     FloodWait
 )
+from flask import send_from_directory
+
+# Добавьте этот route в main.py после инициализации app
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('templates', filename)
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
